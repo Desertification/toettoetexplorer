7520dddec2a61c15c028834df065b6238948f37d
Robin Spruytte <robin.spruytte@hotmail.com>
2017-11-15 14:21:25 +0100

    added interrupt enable for falling edges

--- gpio.h
+++ gpio.h
@@ -33,5 +33,6 @@ void gpio_toggle(uint8_t pin);
 uint8_t gpio_read(uint8_t pin);
 
 void gpio_enable_interrupt_rising_edge(uint8_t pin);
+void gpio_enable_interrupt_falling_edge(uint8_t pin);
 
 #endif /* GPIO_H_ */
