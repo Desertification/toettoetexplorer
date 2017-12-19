1b39af1631841f5abd957277ab75f11c0ef2c41c
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-12-19 08:40:24 +0100

    implemented getters

--- servo.c
+++ servo.c
@@ -12,8 +12,8 @@ static const uint8_t SERVO_PIN = 22;
 
 //====================== forward declarations ======================//
 
-uint8_t angle_to_dutycycle(int8_t angle);
-int8_t dutycycle_to_angle(uint8_t dutycycle);
+static uint8_t angle_to_duty_cycle(int8_t angle);
+static int8_t duty_cycle_to_angle(uint8_t duty_cycle);
 
 //====================== global functions ==========================//
 void servo_init(){
@@ -22,17 +22,18 @@ void servo_init(){
 }
 
 void servo_set_angle(int8_t angle){
-	uint8_t dutycycle = angle_to_dutycycle(angle);
+	uint8_t dutycycle = angle_to_duty_cycle(angle);
 	pwm_set_duty_cycle(SERVO_PIN, dutycycle);
 }
 
 int8_t servo_get_angle(){
-	uint8_t dutycycle = pwm_get_duty_cycle(SERVO_PIN);
+	uint8_t duty_cycle = pwm_get_duty_cycle(SERVO_PIN);
+	return duty_cycle_to_angle(duty_cycle);
 }
 
 //====================== static functions ==========================//
 
-uint8_t angle_to_dutycycle(int8_t angle){
+uint8_t angle_to_duty_cycle(int8_t angle){
 //	-90 = 1ms (left)
 //	0 = 1.5ms (central)
 //	+90 = 2ms (right)
@@ -42,6 +43,9 @@ uint8_t angle_to_dutycycle(int8_t angle){
 	return 255 * (pulse_length/period);
 }
 
-int8_t dutycycle_to_angle(uint8_t dutycycle){
-	//int8_t angle = (int8_t) ((period - 1) * 90) - 90;
+int8_t duty_cycle_to_angle(uint8_t duty_cycle){
+	double period = 1000.0/pwm_get_frequency(); // in ms
+	double pulse_length = period*(duty_cycle/255); // in ms
+
+	return (int8_t) ((pulse_length - 1.0) * 180.0) - 90.0;
 }
