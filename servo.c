a97c6f76c9dde4b753547caabc7cd2c3eb785b14
Thomas Cauwelier <thomascauwelier@gmail.com>
2018-01-03 01:19:59 +0100

    add doc

--- servo.c
+++ servo.c
@@ -10,6 +10,7 @@
 #include "wait.h"
 
 static const uint8_t SERVO_PIN = 22;
+static uint8_t last_angle = 0;
 
 //====================== forward declarations ======================//
 
@@ -17,25 +18,43 @@ static uint8_t angle_to_duty_cycle(int8_t angle);
 static int8_t duty_cycle_to_angle(uint8_t duty_cycle);
 
 //====================== global functions ==========================//
+
+/**
+ * Inits the servo
+ */
 void servo_init(){
 	pwm_init(SERVO_PIN);
 	servo_set_angle(0);
 }
 
+/**
+ * Set the angle of the servo
+ *
+ * @param angle range -90 to 90 degrees
+ */
 void servo_set_angle(int8_t angle){
 	uint8_t dutycycle = angle_to_duty_cycle(angle);
 	pwm_set_duty_cycle(SERVO_PIN, dutycycle);
 	wait_ms(20); // send a few pulses because one pulse is not enough for large changes
 	pwm_set_duty_cycle(SERVO_PIN, 0);
+	last_angle = angle;
 }
 
+/**
+ * Gets the last set angle of the servo
+ *
+ * @return the last set angle of the servo (-90 to 90)
+ */
 int8_t servo_get_angle(){
-	uint8_t duty_cycle = pwm_get_duty_cycle(SERVO_PIN);
-	return duty_cycle_to_angle(duty_cycle);
+	// impossible to get actual current value
+	return last_angle;
 }
 
 //====================== static functions ==========================//
 
+/**
+ * converts the angle to a duty cycle with the appropriate pulse length
+ */
 uint8_t angle_to_duty_cycle(int8_t angle){
 //	-90 = 1ms (left)
 //	0 = 1.5ms (central)
@@ -46,6 +65,9 @@ uint8_t angle_to_duty_cycle(int8_t angle){
 	return 255 * (pulse_length/period);
 }
 
+/**
+ * converts the duty cycle to an angle that corresponds with the pulse length
+ */
 int8_t duty_cycle_to_angle(uint8_t duty_cycle){
 	double period = 1000.0/pwm_get_frequency(); // in ms
 	double pulse_length = period*(duty_cycle/255); // in ms
--- servo.h
+++ servo.h
@@ -10,8 +10,23 @@
 
 #include "LPC17xx.h"
 
+/**
+ * Inits the servo
+ */
 void servo_init();
+
+/**
+ * Set the angle of the servo
+ *
+ * @param angle range -90 to 90 degrees
+ */
 void servo_set_angle(int8_t angle);
+
+/**
+ * Gets the last set angle of the servo
+ *
+ * @return the last set angle of the servo (-90 to 90)
+ */
 int8_t servo_get_angle();
 
 #endif /* SERVO_H_ */
