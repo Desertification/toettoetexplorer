89038478f664f9b025d427ed7fa210655fe8845b
Robin Spruytte <robin.spruytte@hotmail.com>
2018-01-08 17:03:32 +0100

    added docs

--- lcd.h
+++ lcd.h
@@ -10,10 +10,33 @@
 
 #include "LPC17xx.h"
 
+/**
+ * sends a command to the lcd screen to perform an action
+ *
+ * @param value: command to send (commands consist of a 8-bit unsigned integer)
+ */
 void lcd_cmd(uint8_t value);
+
+/**
+ * sends data to put on screen
+ *
+ * @param value: command to send (commands consist of a 8-bit unsigned integer)
+ */
 void lcd_data(uint8_t value);
+
+/**
+ * clear display (also DDRAM content)
+ */
 void lcd_cls(void);
+
+/**
+ * reset lcd
+ */
 void lcd_reset(void);
+
+/**
+ * initialises the lcd
+ */
 void lcd_init(void);
 
 #endif /* LCD_H_ */
